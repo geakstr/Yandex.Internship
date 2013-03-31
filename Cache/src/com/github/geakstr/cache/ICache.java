aabd297940d6015357d3311083168dded99ac300
Dmitry Kharitonov <geakstr@Dmitrys-iMac.local>
2013-03-31 15:26:50 +0400

    Add javadoc for ICache interface

--- Cache/src/com/github/geakstr/cache/ICache.java
+++ Cache/src/com/github/geakstr/cache/ICache.java
@@ -1,9 +1,19 @@
 package com.github.geakstr.cache;
 
 public interface ICache<K, V> {
+	/**
+	 * Получает значение из кэша по ключу key
+	 */
 	V get(Object key);
 
+	/**
+	 * Добавляет в кэш новое значение value, ассоциируя его с ключом key, и
+	 * возвращает это значение
+	 */
 	V put(K key, V value);
 
+	/**
+	 * Проверяет наличие ключа key в кэше
+	 */
 	boolean containsKey(Object key);
 }
